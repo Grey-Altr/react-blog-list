f01ecadd3fa3f76322c2e278142b02213d2dae30
Grey Altr <grey.sifr@gmail.com>
2023-01-17 15:02:00 -0800

    typo correction in blogs.js

--- src/services/blogs.js
+++ src/services/blogs.js
@@ -1,6 +1,6 @@
 import { checkError, client } from './client.js';
 
 export async function getBlogs() {
-  const resp = await client.from('blog').select('*');
+  const resp = await client.from('blogs').select('*');
   return checkError(resp);
 }
